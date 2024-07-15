0cff463c46654d2e147758639ba7ed937f5e2458
Maxwell Antwi <maxwell.antwi@amalitech.com>
2024-07-15 05:33:41 +0100

    fix: refactor fixes

--- lib/screens/mainscreens/details_screen.dart
+++ lib/screens/mainscreens/details_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:kiki/consts/const_widgets.dart';
 import 'package:kiki/functions/sharedpref.dart';
